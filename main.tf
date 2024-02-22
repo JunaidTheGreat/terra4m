8a13eebaad9eceb6564f4fe01d6b959e1fcec6ce
JunaidTheGreat <ijaiyajunaid@yahoo.com>
2024-02-22 20:04:02 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+    region = "eu-west-1"
+}
+
+
+resource "aws_instance" "example" {
+    ami = "ami-0ef9e689241f0bb6e"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = [aws_security_group.instance.id]
+    
+    user_data = <<-EOF
+                #!/bin/bash 
+                echo "Hello, world" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
+
+    user_data_replace_on_change = true
+
+    tags = {
+        Name = "terraform_ec2"
+        }
+    }
+
+resource "aws_security_group" "instance" {
+    name = "terraform-example-instance"
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
